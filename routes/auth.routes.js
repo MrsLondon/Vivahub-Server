186639df4552bd935f1038407e7a19d79139d319
ayat <abuhajayat@gmail.com>
2025-04-04 09:46:32 +0200

    render fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,74 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-const Salon = require("../models/Salon");
+const authController = require("../controllers/auth.controller");
 
 // Register new user
-router.post("/register", async (req, res) => {
-  try {
-    const { email, password, firstName, lastName, role, businessDetails } =
-      req.body;
-    res.status(201).json({
-      message: "User registered successfully",
-      token,
-      user: {
-        id: user._id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        role: user.role,
-        businessDetails: user.businessDetails,
-      },
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error registering user", error: error.message });
-  }
-});
+router.post("/register", authController.register);
 
 // Login user
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Verify if the user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Verify if the password is correct
-    const isPasswordValid = await user.comparePassword(password); // Certifique-se de que o método comparePassword está implementado no modelo User
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid password" });
-    }
-
-    // Generate JWT token
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET, // Certify that we have a JWT_SECRET in our environment variables
-      { expiresIn: "1d" } // Token valid for 1 day
-    );
-
-    // Return success response with token and user details
-    res.json({
-      message: "Login successful",
-      token,
-      user: {
-        id: user._id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        role: user.role,
-        businessDetails: user.businessDetails,
-        createdAt: user.createdAt,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Error logging in", error: error.message });
-  }
-});
+router.post("/login", authController.login);
 
 module.exports = router;
