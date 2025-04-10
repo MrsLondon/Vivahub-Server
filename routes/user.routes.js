6206e410ee9a9c3e19d340300d44609fff0e404d
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2025-04-10 12:08:34 +0200

    delete and update user routes add

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("../controllers/user.controller");
-const { authenticateUser } = require("../middlewares/authMiddleware");
+const {
+  getAllUsers,
+  updateCustomerUser,
+  deleteCustomerUser,
+} = require("../controllers/user.controller");
+const authenticateUser = require("../middlewares/authMiddleware");
+
+// Update customer user
+router.put("/update", authenticateUser, updateCustomerUser);
+
+// Delete customer user
+router.delete("/delete", authenticateUser, deleteCustomerUser);
 
 // Get all users (excluding password)
 router.get("/", getAllUsers);
